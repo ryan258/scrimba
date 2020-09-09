12e36e0ba3d61e2f496aa5f81a61823b5d3ba629
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-09-09 13:05:22 -0500

    Pagination implementation.

--- module-7--apis/beer-app/script.js
+++ module-7--apis/beer-app/script.js
@@ -1,8 +1,12 @@
-const urlBase = "https://api.punkapi.com/v2/beers";
+const urlBase = "https://api.punkapi.com/v2/beers?page=";
 const filterABV = document.getElementById("filterABV");
 const filterIBU = document.getElementById("filterIBU");
+const pageText = document.getElementById("pageNumber");
+const prevPage = document.getElementById("prevPage");
+const nextPage = document.getElementById("nextPage");
 let optionsABV = "";
 let optionsIBU = "";
+let page = 1;
 
 // filters
 filterABV.addEventListener("change", (e) => {
@@ -13,15 +17,17 @@ filterABV.addEventListener("change", (e) => {
       optionsABV = "";
       break;
     case "weak":
-      optionsABV = "abv_lt=4.6";
+      optionsABV = "&abv_lt=4.6";
       break;
     case "medium":
-      optionsABV = "abv_gt=4.5&abv_lt=7.6";
+      optionsABV = "&abv_gt=4.5&abv_lt=7.6";
       break;
     case "strong":
-      optionsABV = "abv_gt=7.5";
+      optionsABV = "&abv_gt=7.5";
       break;
   }
+
+  page = 1;
 
   getBeers();
 });
@@ -34,28 +40,45 @@ filterIBU.addEventListener("change", (e) => {
       optionsIBU = "";
       break;
     case "weak":
-      optionsIBU = "ibu_lt=35";
+      optionsIBU = "&ibu_lt=35";
       break;
     case "medium":
-      optionsIBU = "ibu_gt=34&ibu_lt=75";
+      optionsIBU = "&ibu_gt=34&ibu_lt=75";
       break;
     case "strong":
-      optionsIBU = "ibu_gt=74";
+      optionsIBU = "&ibu_gt=74";
       break;
   }
+
+  page = 1;
 
   getBeers();
 });
 
 async function getBeers() {
   // const url = urlBase + "/";
-  const url = urlBase + "?" + optionsABV + "&" + optionsIBU;
+  const url = urlBase + page + optionsABV + optionsIBU;
   console.log(url);
   // get that beer
   const beerPromise = await fetch(url);
   const beerJSON = await beerPromise.json();
-  console.log(beerJSON[0]);
+  // console.log(beerJSON[0]);
+  // pagination
+  pageText.innerText = page;
 
+  if (page === 1) {
+    prevPage.disabled = true;
+  } else {
+    prevPage.disabled = false;
+  }
+
+  if (beerJSON.length < 25) {
+    nextPage.disabled = true;
+  } else {
+    nextPage.disabled = false;
+  }
+
+  // render data
   const beersDiv = document.querySelector(".beers");
   let beerHTML = "";
 
@@ -87,5 +110,16 @@ async function getBeers() {
   // serve that beer to the page!
   beersDiv.innerHTML = beerHTML;
 }
+
+// pagination
+prevPage.addEventListener("click", () => {
+  page--;
+  getBeers();
+});
+
+nextPage.addEventListener("click", () => {
+  page++;
+  getBeers();
+});
 
 getBeers();
