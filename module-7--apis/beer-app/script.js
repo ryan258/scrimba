eaa65f7f9490aecac67b6f37148ee423b4a3cb0a
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-09-09 10:12:12 -0500

    Set up HTML/CSS for beer cards.

--- module-7--apis/beer-app/script.js
+++ module-7--apis/beer-app/script.js
@@ -4,14 +4,24 @@ async function getBeers() {
   // get that beer
   const beerPromise = await fetch(urlBase);
   const beerJSON = await beerPromise.json();
-  // console.log(beerJSON);
+  console.log(beerJSON[0]);
 
   const beersDiv = document.querySelector(".beers");
   let beerHTML = "";
 
+  // render all those beers
   beerJSON.forEach((beer) => {
     beerHTML += `
-      <h3>${beer.name}</h3>
+    <div class="beer-wrapper card">
+      <div class="beer">
+        <img alt="" src="${beer.image_url}" class="beer__img" />
+        <h3>${beer.name}</h3>
+        <span class="beer__info">
+          <span>ABV: ${beer.abv}%</span>
+          <span>IBU: ${beer.ibu}%</span>
+        </span>
+      </div>
+    </div>
     `;
   });
 
