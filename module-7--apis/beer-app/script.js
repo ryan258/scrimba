78378b9d0fbb37d92be0d7b3e537e0b20da834a3
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-09-09 11:24:20 -0500

    Enable ABV beer filter.

--- module-7--apis/beer-app/script.js
+++ module-7--apis/beer-app/script.js
@@ -1,8 +1,34 @@
 const urlBase = "https://api.punkapi.com/v2/beers";
+const filterABV = document.getElementById("filterABV");
+let optionsABV = "";
+
+// filters
+filterABV.addEventListener("change", (e) => {
+  const value = e.target.value;
+  console.log(value);
+  switch (value) {
+    case "all":
+      optionsABV = "";
+      break;
+    case "weak":
+      optionsABV = "abv_lt=4.6";
+      break;
+    case "medium":
+      optionsABV = "abv_gt=4.5&abv_lt=7.6";
+      break;
+    case "strong":
+      optionsABV = "abv_gt=7.5";
+      break;
+  }
+
+  getBeers();
+});
 
 async function getBeers() {
+  const url = urlBase + "?" + optionsABV;
+  // console.log(url);
   // get that beer
-  const beerPromise = await fetch(urlBase);
+  const beerPromise = await fetch(url);
   const beerJSON = await beerPromise.json();
   console.log(beerJSON[0]);
 
@@ -18,7 +44,7 @@ async function getBeers() {
         <h3>${beer.name}</h3>
         <span class="beer__info">
           <span>ABV: ${beer.abv}%</span>
-          <span>IBU: ${beer.ibu}%</span>
+          <span>IBU: ${beer.ibu}</span>
         </span>
       </div>
       <div class="beer__content">
