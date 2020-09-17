ffa75427c6a5f04e284a70eab6569597a75c8924
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-09-17 11:15:22 -0500

    Set up Jeopardy board with fetched categories.

--- module-8--apps-w-flexbox-and-grid/section-grid-and-fetch/jeopardy/index.js
+++ module-8--apps-w-flexbox-and-grid/section-grid-and-fetch/jeopardy/index.js
@@ -10,3 +10,31 @@
         in a simplified 4x5 Jeopardy Board 
         using CSS Grid
 */
+
+async function getCategories() {
+  let response = await fetch(
+    "https://jservice.io/api/categories?count=4&offset=4"
+  );
+  let data = await response.json();
+  return data;
+  // console.log(data);
+}
+
+function getCategoryHTML(category) {
+  return `
+    <div class="my-category-title">${category.title}</div>
+    <div class="my-category-clue" style="grid-row-start: 2">$100</div>
+    <div class="my-category-clue" style="grid-row-start: 3">$200</div>
+    <div class="my-category-clue" style="grid-row-start: 4">$300</div>
+    <div class="my-category-clue" style="grid-row-start: 5">$400</div>
+  `;
+}
+
+getCategories().then((categories) => {
+  console.log(categories);
+  document.body.innerHTML = `
+    <div class="board">
+      ${categories.map(getCategoryHTML).join("")}
+    </div>
+  `;
+});
