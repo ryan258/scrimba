2dafda7374f6572977d5b22b230a560c0c249804
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-09-17 13:23:45 -0500

    Starting point for the library example.

---
+++ module-8--apps-w-flexbox-and-grid/section-grid-and-fetch/library/index.js
@@ -0,0 +1,12 @@
+/* 
+    Library
+    
+    Fetch the collection of books 
+        from books.json, assign each 
+        a unique id
+        
+    Create a div to represent a book
+        using Grid & grid-template-areas 
+        build a book component
+        display all books using Flexbox
+*/
