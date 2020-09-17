532a98193f1fa2e18eb4cac1630efa2064ea65f3
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-09-17 13:19:41 -0500

    Complete Pokedex challenges.

--- module-8--apps-w-flexbox-and-grid/section-grid-and-fetch/pokedex/index.js
+++ module-8--apps-w-flexbox-and-grid/section-grid-and-fetch/pokedex/index.js
@@ -11,6 +11,17 @@
         Stats: HP/Attack/Defense/Speed
 */
 
+/*
+    Pokedex (Challenge)
+    
+    Write and utilize a 
+        displayPokedex(allPokemon) function 
+    
+    Display the Japanese, Chinese, and French
+        name of each Pokemon under its Stats
+        each should take up 1/3rd of the row 
+*/
+
 async function getAllPokemon() {
   let response = await fetch("pokemon.json");
   let data = await response.json();
@@ -28,14 +39,27 @@ function getPokemonHtml(aPokemon) {
     <div class="a-pokemon-stat">Attack: ${aPokemon.base.Attack}</div>
     <div class="a-pokemon-stat">Defense: ${aPokemon.base.Defense}</div>
     <div class="a-pokemon-stat">Speed: ${aPokemon.base.Speed}</div>
+
+    <div class="a-pokemon-alt-name">${aPokemon.name.japanese}</div>
+    <div class="a-pokemon-alt-name">${aPokemon.name.chinese}</div>
+    <div class="a-pokemon-alt-name">${aPokemon.name.french}</div>
   </div>`;
 }
 
-getAllPokemon().then((allPokemon) => {
-  let samplePokemon = allPokemon[350];
-  console.log(samplePokemon);
-  // document.body.innerHTML = getPokemonHtml(samplePokemon);
+function displayPokedex(allPokemon) {
+  console.log(allPokemon[0]);
   document.body.innerHTML = `<div class="my-pokedex">
-    ${allPokemon.map((aPokemon) => getPokemonHtml(aPokemon)).join("")}
+      ${allPokemon.map(getPokemonHtml).join("")}
   </div>`;
-});
+}
+
+getAllPokemon().then(displayPokedex);
+
+// getAllPokemon().then((allPokemon) => {
+//   let samplePokemon = allPokemon[350];
+//   console.log(samplePokemon);
+//   // document.body.innerHTML = getPokemonHtml(samplePokemon);
+//   document.body.innerHTML = `<div class="my-pokedex">
+//     ${allPokemon.map((aPokemon) => getPokemonHtml(aPokemon)).join("")}
+//   </div>`;
+// });
