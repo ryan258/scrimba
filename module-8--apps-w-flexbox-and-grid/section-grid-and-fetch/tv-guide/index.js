afe30ea9ebbcc9a099139570b99ffe43935353e4
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-09-18 09:03:25 -0500

    Develop the TV guide exercise.

---
+++ module-8--apps-w-flexbox-and-grid/section-grid-and-fetch/tv-guide/index.js
@@ -0,0 +1,38 @@
+/* 
+    TV Guide
+    
+    Write an async function called findShow(query)
+        performs a fetch call to:
+        https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons
+        and returns the resulting show object
+        
+    Build a layout to display the show
+        - Title
+        - Summary
+        - Seasons listed as individual divs
+*/
+
+async function findShow(query) {
+  let response = await fetch(
+    `https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons`
+  );
+  let data = response.json();
+  return data;
+}
+
+findShow("office").then((show) => {
+  console.log(show);
+
+  document.body.innerHTML = `
+    <div class="my-show">
+      <div class="my-show-title">${show.name}</div>
+      <div class="my-show-summary">${show.summary}</div>
+      ${show._embedded.seasons
+        .map((season) => {
+          // console.log(season);
+          return `<div class="my-show-season">Season ${season.number}</div>`;
+        })
+        .join("")}
+    </div>
+  `;
+});
